070d771dbb9697379368340b78c466187925bc2e
Maxwell VJ <maxwell.jimenez@globant.com>
2022-07-19 15:08:20 -0600

    changes

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemsList
 from resources.store import Store, StoreList
-from db import db
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 api = Api(app) # In charge to registry the endpoints
 
 app.secret_key = 'SUPER-SECRET'
@@ -28,4 +28,13 @@ api.add_resource(StoreList, '/stores')
 
 
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app) # Initialize SQLAlchemy to ensure that we can use it
+
+    if app.config['DEBUG']:
+        @app.before_first_request # Call this function automatically before the first request
+        def create_tables():
+            db.create_all() # SQLAlchemy create the DB for us automatically
+            
+    
     app.run(port=5000)
--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-import imp
 from app import app
 from db import db
 
 db.init_app(app) # Initialize SQLAlchemy to ensure that we can use it
+
 @app.before_first_request # Call this function automatically before the first request
 def create_tables():
     db.create_all() # SQLAlchemy create the DB for us automatically
